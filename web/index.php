edd8ba0f0d642480ba915786419769a8b2814750
hhs <hhs@DESKTOP-SSQTKEN>
2018-12-27 21:44:32 +0800

    修改配置文件1

--- web/index.php
+++ web/index.php
@@ -1,16 +1,20 @@
 <?php
 //在php.ini中开启时区，date.timezone=Asia/Shanghai  并没什么卵用
 ini_set('date.timezone','Asia/Shanghai');//如果PHP.INI没有指定,在此位临时声明也可以
+$isdebug = $_SERVER['SERVER_NAME'] == '4gnote.com'?false:true;
 //true=测试环境，false=正式环境
-if(true){
+if($isdebug){
     define("ISALIYUN", false);
     defined('YII_DEBUG') or define('YII_DEBUG', false);  //上线要改
     defined('YII_ENV') or define('YII_ENV', 'dev');  //上线要改
+    require(__DIR__ . '../vendor/autoload.php');
+    require(__DIR__ . '../vendor/yiisoft/yii2/Yii.php');
 }else{
     define("ISALIYUN", true);
+    require(__DIR__ . '../../yii2/vendor/autoload.php');
+    require(__DIR__ . '../../yii2/vendor/yiisoft/yii2/Yii.php');
 }
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
 $config = require(__DIR__ . '/../config/web.php');
 //加入版本号 RELEASE_VERSION
 if(file_exists(__DIR__."/../../release_version/sxyz_version")){
